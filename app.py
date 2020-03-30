7f024785af49bb188955f4b4f941d9f2e09cbbd9
kylin-zhuo <lin.zhuo@hotmail.com>
2020-03-29 18:36:03 -0700

    Add htmls.

--- app.py
+++ app.py
@@ -6,7 +6,23 @@ Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name="kylin")
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog.html')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == "__main__":
