37a16ea0ed12ad838fd68fa3cd446a45b2a938cd
kylin-zhuo <lin.zhuo@hotmail.com>
2020-09-11 20:54:49 -0700

    update demi

--- app.py
+++ app.py
@@ -1,46 +1,56 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import json
+import datetime
 
 app = Flask(__name__)
 Bootstrap(app)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/index.html')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about.html')
 def about():
     return render_template('about.html')
 
+
 @app.route('/blog.html')
 def blog():
     return render_template('blog.html')
+
 
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/gallery.html')
 def gellery():
     return render_template('gallery.html')
 
+
 @app.route('/demidovich.html')
 def demodovich():
 	solve = []
-	with open("data/demidovich.txt") as file:
+	with open("data/demidovich/solved.txt") as file:
 		solved = parse_demi_progress(file)
 	return render_template('demidovich.html', solved=solved)
+
 
 @app.route('/update-demi/<string:action>/<int:question_id>')
 def update(action, question_id):
 
 	qs = []
 	
-	with open("data/demidovich.txt", "r") as file:
+	with open("data/demidovich/solved.txt", "r") as file:
 
 		qs = parse_demi_progress(file)
 
@@ -49,23 +59,49 @@ def update(action, question_id):
 
 			if question_id not in qs:
 				qs.append(question_id)
+				qs.sort()
 
 		elif action == 'remove':
 			print("remove" + str(question_id))
 			if question_id in qs:
 				qs.remove(question_id)
 
-	with open("data/demidovich.txt", "w") as file:
+	with open("data/demidovich/solved.txt", "w") as file:
 		file.write(str(qs))
 
-	return "success - " + action + ": " + str(question_id)
- 
+	if action == 'add':
+		logs = {}
+		with open("data/demidovich/log.txt", "r") as log:
+			logs = json.loads(log.read())
+			date_str = str(datetime.datetime.now().date())
+			if date_str not in logs:
+				logs[date_str] = str(question_id)
+			else:
+				cur = logs[date_str]
+				s = cur.split(",")
+				if str(question_id) not in s:
+					s.append(str(question_id))
+				logs[date_str] = ",".join(s)
 
+		with open("data/demidovich/log.txt", "w") as log:
+			log.write(json.dumps(logs))
+
+	return "success - " + action + ": " + str(question_id)
+
+
+@app.route('/test')
+def test1():
+	data = {}
+	with open("data/demidovich/log.json", "r") as file:
+		data = json.loads(file.read())
+	data['2020-09-12'] = "4,5"
+	with open("data/demidovich-log.json", "w") as file:
+		file.write(json.dumps(data))
+	return str(data)
+
+ 
 def parse_demi_progress(f):
-	d = str(f.readline())
-	print(d)
-	d = d.strip('[').strip(']').strip()
-	return [] if not d else list(set(map(lambda x:int(x), d.split(','))))
+	return json.loads(f.read())
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
